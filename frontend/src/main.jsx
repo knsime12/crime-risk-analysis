4f539fbeebd16738064c6bca8bb6bac83cbd958a
knsime12 <knsime12@gmail.com>
2026-07-11 12:51:17 +0900

    홈 화면 스타일 파일 분리

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/base.css'
 import './styles/layout.css'
+import './styles/home.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
