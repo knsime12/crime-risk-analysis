84ced741cf40e1323eb59b788e06fa1e84d7ec98
knsime12 <knsime12@gmail.com>
2026-07-11 12:55:54 +0900

    지역 검색 스타일 파일 분리

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles/base.css'
 import './styles/layout.css'
 import './styles/home.css'
+import './styles/region.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
