b30d3f2260142c49b232f9c43a142465a8188ebe
knsime12 <knsime12@gmail.com>
2026-07-12 15:56:34 +0900

    refactor: Header 네비게이션을 라우터 링크로 변경

--- frontend/src/pages/MapPage.jsx
+++ frontend/src/pages/MapPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 import { getMapRegion } from '../api/client'
 import Header from '../components/Header'
