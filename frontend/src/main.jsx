868e068b54ec582d601a7a32c6aa1152f7615b58
knsime12 <knsime12@gmail.com>
2026-07-12 13:18:54 +0900

    지도 스타일 파일 분리

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import './styles/home.css'
 import './styles/region.css'
 import './styles/common.css'
 import './styles/report.css'
+import './styles/map.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
